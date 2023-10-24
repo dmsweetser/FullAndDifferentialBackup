4ae29bc36cc5aaef0cd1e2f5691c8dcd44703a32
Daniel Sweetser <37463869+dmsweetser@users.noreply.github.com>
2023-10-24 06:03:53 -0400

    Additional fixes

--- Backup.ps1
+++ Backup.ps1
@@ -2,51 +2,53 @@
 $sourceDirectories = @("C:\Files1", "C:\Files2")  # List of source directories to back up
 $backupLocation = "H:\Backups"  # Updated target location for backups
 $backupFrequency = 7  # Days between full backups
+$maxFullBackups = 5    # Maximum number of full backups to keep
 
-# Create a unique backup folder based on the current date
-$backupFolder = Join-Path -Path $backupLocation -ChildPath (Get-Date -Format "yyyyMMdd")
-New-Item -ItemType Directory -Path $backupFolder -Force
-
-# Function to create the 'FullBackup' folder and perform a full backup
+# Function to create a full backup
 function FullBackup {
-    $fullBackupFolder = Join-Path -Path $backupFolder -ChildPath "FullBackup"
-    New-Item -ItemType Directory -Path $fullBackupFolder -Force  # Create FullBackup folder
+    $fullBackupFolder = Join-Path -Path $backupLocation -ChildPath ((Get-Date -Format "yyyyMMdd_HHmmss") + "_Full")
+    New-Item -ItemType Directory -Path $fullBackupFolder -Force
     Copy-Item -Path $sourceDirectories -Destination $fullBackupFolder -Recurse
 }
 
-# Function to perform a differential backup
+# Function to create a differential backup
 function DifferentialBackup {
-    $diffBackupFolder = Join-Path -Path $backupFolder -ChildPath "DifferentialBackup"
-    New-Item -ItemType Directory -Path $diffBackupFolder -Force  # Create DifferentialBackup folder
-    Copy-Item -Path $sourceDirectories -Destination $diffBackupFolder -Recurse -Force
-}
-
-# Function to clean up old backups when disk space is low
-function CleanUpBackups {
-    $backupDrive = [System.IO.Path]::GetPathRoot($backupLocation).TrimEnd('\').TrimEnd(':')
-    while ((Get-PSDrive -Name $backupDrive).Free -lt 1gb) {
-        $backups = Get-ChildItem -Path $backupLocation | Sort-Object CreationTime
-        $oldestBackup = $backups[0]
-        Remove-Item -Path $oldestBackup.FullName -Recurse -Force
-    }
-}
-
-# Check if it's time for a full backup
-if (!(Test-Path $backupFolder)) {
-    FullBackup
-} else {
-    $lastFullBackup = Get-ChildItem -Path (Join-Path -Path $backupFolder -ChildPath "FullBackup") -ErrorAction SilentlyContinue
-    if ($lastFullBackup -eq $null) {
-        FullBackup
-    } else {
-        $daysSinceLastFullBackup = [math]::Round((Get-Date - $lastFullBackup.CreationTime).TotalDays)  # Calculate the days difference
-        if ($daysSinceLastFullBackup -ge $backupFrequency) {
-            FullBackup
-        } else {
-            DifferentialBackup
+    $fullBackups = Get-ChildItem -Path $backupLocation -Filter "*_Full" | Sort-Object CreationTime
+    $lastFullBackup = $fullBackups | Select-Object -Last 1
+    if ($lastFullBackup -ne $null) {
+        $diffBackupFolder = Join-Path -Path $backupLocation -ChildPath ((Get-Date -Format "yyyyMMdd_HHmmss") + "_Diff")
+        New-Item -ItemType Directory -Path $diffBackupFolder -Force
+        $differentialItems = Compare-Object -ReferenceObject (Get-ChildItem -Path $sourceDirectories -File) -DifferenceObject (Get-ChildItem -Path $lastFullBackup.FullName -File)
+        $differentialItems | ForEach-Object {
+            $sourcePath = $_.InputObject.FullName
+            $relativePath = $sourcePath.Substring($sourcePath.IndexOf($lastFullBackup.FullName) + $lastFullBackup.FullName.Length + 1)
+            $destinationPath = Join-Path -Path $diffBackupFolder -ChildPath $relativePath
+            Copy-Item -Path $sourcePath -Destination $destinationPath
         }
     }
 }
 
-# Clean up old backups if disk space is low
-CleanUpBackups
+# Function to clean up old backups if they exceed the maximum number of full backups
+function CleanUpBackups {
+    $fullBackups = Get-ChildItem -Path $backupLocation -Filter "*_Full" | Sort-Object CreationTime
+    $excessFullBackups = $fullBackups | Select-Object -Skip $maxFullBackups
+    $excessFullBackups | ForEach-Object {
+        Remove-Item -Path $_.FullName -Recurse -Force
+    }
+}
+
+# Check if it's time for a full backup or differential backup
+$fullBackups = Get-ChildItem -Path $backupLocation -Filter "*_Full" | Sort-Object CreationTime
+$lastFullBackup = $fullBackups | Select-Object -Last 1
+if ($lastFullBackup -eq $null) {
+    FullBackup
+} else {
+    $lastFullBackupTime = $lastFullBackup.CreationTime
+    $daysSinceLastFullBackup = [math]::Round((Get-Date - $lastFullBackupTime).TotalDays)  # Calculate the days difference
+    if ($daysSinceLastFullBackup -ge $backupFrequency) {
+        FullBackup
+        CleanUpBackups
+    } else {
+        DifferentialBackup
+    }
+}
