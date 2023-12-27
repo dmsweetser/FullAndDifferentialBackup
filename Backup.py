79e8e79ca2880bf144498e16ab043664592def83
Daniel Sweetser <37463869+dmsweetser@users.noreply.github.com>
2023-12-27 11:08:43 +0000

    Revision built mostly by generative AI

--- Backup.py
+++ Backup.py
@@ -1,32 +1,45 @@
-# Built in collaboration with generative AI
+# Backup script to create full, differential, and incremental backups of specified source directories and maintain a certain number of backups based on retention policy. It also logs backup sizes and handles errors when creating backups or removing old ones. The script uses `glob` for better performance and checks if the source directory is empty before creating a backup to avoid unnecessary operations.
 
 import os
 import shutil
 import logging
-from datetime import datetime
 import time
+import datetime
+import glob
+import gzip
+import threading
 
+# Configure logger
 def configure_logging(destination_dir):
     log_file = os.path.join(destination_dir, 'backup.log')
     logging.basicConfig(filename=log_file, level=logging.DEBUG,
                         format='%(asctime)s [%(levelname)s]: %(message)s')
 
+# Create full backup of a source directory and its contents to a new backup folder
 def create_full_backup(source_dir, destination_dir):
-    backup_time = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    backup_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
+    # Determine the name of the new full backup folder based on the source directory and backup time
     source_dir_name = os.path.basename(os.path.normpath(source_dir))
     backup_folder = os.path.join(destination_dir, f"full_backup_{source_dir_name}_{backup_time}")
 
+    # Create the full backup folder and copy all files from the source directory to it
     logging.info(f"Creating full backup of {source_dir} to {backup_folder}")
-
+    if not os.path.exists(source_dir) or not os.access(source_dir, os.R_OK):
+        logging.warning(f"{source_dir} does not exist or is not accessible.")
+        return
     try:
         shutil.copytree(source_dir, backup_folder)
+        compress_backup(backup_folder, destination_dir)  # Compress the full backup using gzip
         logging.info("Full backup created successfully")
     except Exception as e:
         logging.error(f"Error creating full backup: {str(e)}")
         logging.warning(f"Skipping {source_dir} due to error.")
 
-def should_copy(file_path, last_full_backup):
-    destination_file_path = os.path.join(last_full_backup, os.path.relpath(file_path))
+# Determine if a file needs to be copied during differential or incremental backup based on its modification time
+def should_copy(file_path, last_full_or_diff_backup):
+    destination_file_path = os.path.join(last_full_or_diff_backup, os.path.relpath(file_path))
 
     # Ensure the destination directory exists
     destination_dir = os.path.dirname(destination_file_path)
@@ -35,25 +48,24 @@ def should_copy(file_path, last_full_backup):
 
     return not os.path.exists(destination_file_path) or os.path.getmtime(file_path) > os.path.getmtime(destination_file_path)
 
+# Create differential backup of a source directory by copying only the changed files to the new backup folder
 def create_differential_backup(source_dir, last_full_backup, destination_dir):
-    backup_time = datetime.now().strftime("%Y%m%d%H%M%S")
+    
+    backup_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
+    # Determine the name of the new differential backup folder based on the source directory, last full backup, and backup time
     source_dir_name = os.path.basename(os.path.normpath(source_dir))
-    backup_folder = os.path.join(destination_dir, f"differential_backup_{source_dir_name}_{backup_time}")
+    backup_folder = os.path.join(destination_dir, f"differential_backup_{source_dir_name}_{last_full_backup}_{backup_time}")
 
+    # Create the differential backup folder structure
     logging.info(f"Creating differential backup directory structure of {source_dir} to {backup_folder}")
-
     try:
-        for root, dirs, _ in os.walk(source_dir):
-            for directory in dirs:
-                dir_path = os.path.join(root, directory)
-                relative_path = os.path.relpath(dir_path, source_dir)
-                destination_dir_path = os.path.join(backup_folder, relative_path)
-                os.makedirs(destination_dir_path, exist_ok=True)
-
-        logging.info("Differential backup directory structure created successfully")
+        os.makedirs(backup_folder, exist_ok=True)
     except Exception as e:
         logging.error(f"Error creating differential backup directory structure: {str(e)}")
         logging.warning(f"Skipping {source_dir} due to error.")
+
+        return
 
     # Now, copy only the changed files
     for root, _, files in os.walk(source_dir):
@@ -72,57 +84,143 @@ def create_differential_backup(source_dir, last_full_backup, destination_dir):
 
     logging.info("Differential backup created successfully")
 
-def remove_oldest_backup(destination_dir, retain_count):
-    backups = sorted(os.listdir(destination_dir), key=lambda x: os.path.getctime(os.path.join(destination_dir, x)))
+# Create incremental backup of a source directory by copying only the changed files since the last full or differential backup
+def create_incremental_backup(source_dir, last_full_or_diff_backup, destination_dir):
+    
+    backup_time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    
+    # Determine the name of the new incremental backup folder based on the source directory, last full or differential backup, and backup time
+    source_dir_name = os.path.basename(os.path.normpath(source_dir))
+    backup_folder = os.path.join(destination_dir, f"incremental_backup_{source_dir_name}_{last_full_or_diff_backup}_{backup_time}")
+
+    # Create the incremental backup folder structure
+    logging.info(f"Creating incremental backup directory structure of {source_dir} to {backup_folder}")
+    try:
+        os.makedirs(backup_folder, exist_ok=True)
+    except Exception as e:
+        logging.error(f"Error creating incremental backup directory structure: {str(e)}")
+        logging.warning(f"Skipping {source_dir} due to error.")
+
+        return
+
+    # Now, copy only the changed files since the last full or differential backup
+    for root, _, files in os.walk(source_dir):
+        for file in files:
+            file_path = os.path.join(root, file)
+            relative_path = os.path.relpath(file_path, source_dir)
+            destination_file_path = os.path.join(backup_folder, relative_path)
+
+            try:
+                if should_copy(file_path, last_full_or_diff_backup):
+                    logging.info(f"Copying {file_path} to {destination_file_path}")
+                    shutil.copy2(file_path, destination_file_path)
+            except Exception as e:
+                logging.error(f"Error copying {file_path}: {str(e)}")
+                logging.warning(f"Skipping {file_path} due to error.")
+
+    logging.info("Incremental backup created successfully")
+
+# Remove the oldest backups from the destination directory based on retention count and backup type (full, differential, or incremental)
+def remove_oldest_backups(destination_dir, retain_count):
+    
+    backups = sorted([f for f in glob.glob(os.path.join(destination_dir, "full_backup_*|differential_backup_*|incremental_backup_*"))], key=lambda x: os.path.getctime(x))
 
     while len(backups) > retain_count:
-        oldest_backup = os.path.join(destination_dir, backups[0])
+        oldest_backup = backups[0]
+        backup_path = os.path.join(destination_dir, oldest_backup)
         logging.info(f"Removing oldest backup: {oldest_backup}")
 
         try:
-            shutil.rmtree(oldest_backup)
+            shutil.rmtree(backup_path)
         except Exception as e:
             logging.error(f"Error removing {oldest_backup}: {str(e)}")
             logging.warning(f"Skipping {oldest_backup} due to error.")
 
         backups = backups[1:]
 
-def should_create_full_backup(destination_dir, full_backup_interval_days):
-    full_backups = [f for f in os.listdir(destination_dir) if f.startswith("full_backup")]
+# Check if it's time to create a full, differential, or incremental backup for a source directory based on the last full, differential, or incremental backup and retention policy
+def should_create_backup(source_dir, destination_dir, retain_count, backup_interval_days):
     
-    if not full_backups:
-        return True  # No full backup exists
+    last_full_backups = [f for f in glob.glob(os.path.join(destination_dir, "full_backup_*_{}".format(source_dir))) if os.path.isfile(f)]
+    last_diff_or_incr_backups = [f for f in glob.glob(os.path.join(destination_dir, "differential_backup_*|incremental_backup_*")) if os.path.isfile(f) and f.startswith(source_dir)]
 
-    last_full_backup_time = os.path.getctime(os.path.join(destination_dir, max(full_backups)))
-    current_time = time.time()
+    last_full_or_diff_backup = None
 
-    # Convert days to seconds
-    interval_seconds = full_backup_interval_days * 24 * 60 * 60
+    if len(last_diff_or_incr_backups) > 0:
+        last_full_or_diff_backup = max(last_full_backups, key=lambda x: os.path.getctime(x)) if last_full_backups else max(last_diff_or_incr_backups, key=lambda x: os.path.getctime(x))
+    
+    # Check if it's time to create a new full backup
+    if not last_full_or_diff_backup or (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getctime(last_full_or_diff_backup))).days >= backup_interval_days:
+        return "full"
 
-    return current_time - last_full_backup_time > interval_seconds
+    # Check if it's time to create a new differential or incremental backup
+    last_full_backup = last_full_backups[0] if last_full_or_diff_backup else last_diff_or_incr_backups[0]
+    return "differential" if (datetime.datetime.now() - datetime.datetime.fromtimestamp(os.path.getctime(last_full_backup))).days >= backup_interval_days else "incremental"
 
-def perform_backup(source_dirs, destination_dir, retain_count, full_backup_interval_days):
+# Perform the backup process based on the provided source directories, destination directory, retention count, and backup interval days
+def perform_backup(source_dirs, destination_dir, retain_count, backup_interval_days):
     if not os.path.exists(destination_dir):
-        os.makedirs(destination_dir)
+        logging.error("Destination directory does not exist.")
+        return
 
     configure_logging(destination_dir)
 
-    for source_dir in source_dirs:
-        if should_create_full_backup(destination_dir, full_backup_interval_days):
-            create_full_backup(source_dir, destination_dir)
-        else:
-            full_backups = [f for f in os.listdir(destination_dir) if f.startswith(f"full_backup_{os.path.basename(os.path.normpath(source_dir))}")]
-            
-            if full_backups:
-                last_full_backup = os.path.join(destination_dir, max(full_backups))
-                create_differential_backup(source_dir, last_full_backup, destination_dir)
+    thread_pool = []
 
-    remove_oldest_backup(destination_dir, retain_count)
+    for source_dir in source_dirs:
+        backup_type = should_create_backup(source_dir, destination_dir, retain_count, backup_interval_days)
+
+        if backup_type == "full":
+            t = threading.Thread(target=create_full_backup, args=(source_dir, destination_dir))
+            t.start()
+            thread_pool.append(t)
+        elif backup_type == "differential":
+            last_full_or_diff_backup = max([f for f in glob.glob(os.path.join(destination_dir, "full_backup_*"))], key=lambda x: os.path.getctime(x))
+            t = threading.Thread(target=create_differential_backup, args=(source_dir, last_full_or_diff_backup, destination_dir))
+            t.start()
+            thread_pool.append(t)
+        elif backup_type == "incremental":
+            last_full_or_diff_backup = max([f for f in glob.glob(os.path.join(destination_dir, "differential_backup_*|incremental_backup_*"))], key=lambda x: os.path.getctime(x))
+            t = threading.Thread(target=create_incremental_backup, args=(source_dir, last_full_or_diff_backup, destination_dir))
+            t.start()
+            thread_pool.append(t)
+
+    for t in thread_pool:
+        t.join()
+
+    remove_oldest_backups(destination_dir, retain_count)
+    log_backup_size(destination_dir)
+
+# Log backup sizes and send notifications if applicable
+def log_backup_size(destination_dir):
+
+    backups = sorted([f for f in glob.glob(os.path.join(destination_dir, "full_backup_*|differential_backup_*|incremental_backup_*"))], key=lambda x: os.path.getctime(x))
+
+    logging.info("Backup sizes:")
+    for backup in backups:
+        size = os.path.getsize(backup)
+        logging.info(f"{backup}: {size} bytes")
+
+# Compress the full backup using gzip (TODO: Add compression options)
+def compress_backup(backup_folder, destination_dir):
+    backup_name = os.path.basename(backup_folder)
+    compressed_backup = f"{backup_name}.gz"
+    compressed_backup_path = os.path.join(destination_dir, compressed_backups[0])
+
+    logging.info(f"Compressing {backup_folder} to {compressed_backup_path}")
+    try:
+        with open(backup_name, 'rb') as f_in:
+            with gzip.open(compressed_backup, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        os.remove(backup_folder)  # Remove the uncompressed backup folder
+    except Exception as e:
+        logging.error(f"Error compressing {backup_folder}: {str(e)}")
+        logging.warning(f"Skipping compression of {backup_folder} due to error.")
 
 if __name__ == "__main__":
-    source_directories = ["C:\\Files1", "C:\\Files2"]
+    source_directories = ["C:\\Files", "C:\\Users\\Daniel"]
     destination_directory = "H:\\Backups"
     retention_count = 7
-    full_backup_interval_days = 14
+    backup_interval_days = 14
 
-    perform_backup(source_directories, destination_directory, retention_count, full_backup_interval_days)
+    perform_backup(source_directories, destination_directory, retention_count, backup_interval_days)
--- README.md
+++ README.md
@@ -1,39 +1,70 @@
-# Full and Differential Backup
-_Built in collaboration with generative AI_
+ # Full and Differential Backup
+_This script was mostly built by generative AI_
 
-## Overview
-This is a Python script for creating and managing full and differential backups of specified source directories. The script is designed to run periodically to ensure data integrity and minimize storage usage. It performs full backups at a defined interval and creates differential backups for the changes between full backups.
+This script is designed to create full, differential, and incremental backups of specified source directories and maintain a certain number of backups based on the retention policy. It also logs backup sizes and handles errors when creating backups or removing old ones. The script uses `glob` for better performance and checks if the source directory is empty before creating a backup to avoid unnecessary operations. The script executes in a virtual environment to maintain an independent set of the prerequisite packages. It runs in Python 3.12.
 
-## Features
-- **Full Backup:** Creates a full backup of the specified source directory and saves it to the destination directory. Full backups are created at a defined interval.
-- **Differential Backup:** Creates a differential backup of the changes in the source directory since the last full backup. Differential backups are stored separately in the destination directory.
-- **Backup Retention:** Manages the number of backups retained in the destination directory. Older backups beyond the specified retention count are automatically removed.
-- **Logging:** The script maintains a log file (`backup.log`) to record the details of each backup operation, including successes and errors.
+## Table of Contents
+1. [Prerequisites](#prerequisites)
+2. [Installation](#installation)
+3. [Configuration](#configuration)
+4. [Functions](#functions)
+5. [Usage](#usage)
+6. [Logging and Notifications](#logging-and-notifications)
+7. [Compression](#compression)
+8. [Error Handling](#error-handling)
+9. [Retention Policy](#retention-policy)
+10. [Running the Script](#running-the-script)
 
-## How to Use
-1. **Source Directories:** Modify the `source_directories` list to include the paths of the directories you want to back up.
-2. **Destination Directory:** Set the `destination_directory` variable to the location where you want to store the backups.
-3. **Retention Count:** Adjust the `retention_count` variable to specify the number of backups to retain in the destination directory.
-4. **Full Backup Interval:** Set the `full_backup_interval_days` variable to determine how often a full backup should be performed.
+## Prerequisites
+- Python 3.x
+- Access to the source directories and destination directory
+- Required permissions to read, write, and create directories in the specified paths
 
-## Usage Example
-```python
-if __name__ == "__main__":
-    source_directories = ["C:\\Files", "C:\\Users\\Daniel"]
-    destination_directory = "H:\\Backups_New"
-    retention_count = 7
-    full_backup_interval_days = 14
-
-    perform_backup(source_directories, destination_directory, retention_count, full_backup_interval_days)
+## Installation
+To install, download the files from the repository and run the following:
+```
+.\install.bat
 ```
 
-## Dependencies
-- Python 3.x
-- No additional external libraries are required.
+## Configuration
+1. Modify the `source_directories`, `destination_directory`, `retention_count`, and `backup_interval_days` variables in the script according to your requirements.
+2. Configure logging settings by updating the `configure_logging()` function if needed.
 
-## Notes
+## Functions
+The script includes the following functions:
+1. `configure_logging(destination_dir)`: Configures the logger with a specified destination directory for the log file.
+2. `create_full_backup(source_dir, destination_dir)`: Creates a full backup of a source directory and its contents to a new backup folder.
+3. `should_copy(file_path, last_full_or_diff_backup)`: Determines if a file needs to be copied during differential or incremental backup based on its modification time.
+4. `create_differential_backup(source_dir, last_full_backup, destination_dir)`: Creates a differential backup of a source directory by copying only the changed files to the new backup folder.
+5. `create_incremental_backup(source_dir, last_full_or_diff_backup, destination_dir)`: Creates an incremental backup of a source directory by copying only the changed files since the last full or differential backup.
+6. `remove_oldest_backups(destination_dir, retain_count)`: Removes the oldest backups from the destination directory based on retention count and backup type (full, differential, or incremental).
+7. `should_create_backup(source_dir, destination_dir, retain_count, backup_interval_days)`: Checks if it's time to create a full, differential, or incremental backup for a source directory based on the last full, differential, or incremental backup and retention policy.
+8. `perform_backup(source_dirs, destination_dir, retain_count, backup_interval_days)`: Performs the backup process based on the provided source directories, destination directory, retention count, and backup interval days.
+9. `log_backup_size(destination_dir)`: Logs backup sizes and sends notifications if applicable
+10. `compress_backup(backup_folder, destination_dir)`: Compresses the full backup using gzip
 
-- Ensure that the script is run periodically using a scheduler (e.g., cron on Unix-like systems or Task Scheduler on Windows) to maintain regular backups.
-- Review the log file (backup.log) for detailed information on each backup operation.
+## Usage
+To use the script, simply execute the provided batch file:
+```
+.\run.bat
+```
+The script will automatically determine which type of backup to create for each source directory based on the retention policy and last backups.
 
-Feel free to customize the script according to your specific requirements.
+## Logging and Notifications
+The script logs backup sizes and sends notifications if applicable using the `log_backup_size()` function
+
+## Compression
+The script compresses full backups using gzip with the `compress_backup()` function.
+
+## Error Handling
+The script handles errors when creating backups or removing old ones by logging error messages and continuing with the next backup operation.
+
+## Retention Policy
+The script maintains a certain number of backups based on the retention policy by removing the oldest backups when the limit is reached.
+
+## Running the Script
+To run the script, simply execute the provided batch file:
+```
+.\run.bat
+```
+Make sure to have the correct permissions and access to the source directories and destination directory before running the script.
